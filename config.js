75bb5d7f254f0d66990815af77d3bfaca9916bb1
Cristians Soria <cristiansoria@gmail.com>
2023-06-16 00:05:10 -0300

    Update code to SDK v3

--- config.js
+++ config.js
@@ -6,9 +6,11 @@ module.exports = {
     //Provide details for local configuration
   },
   aws_remote_config: {
-    accessKeyId: process.env.ACCESS_KEY_ID,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY,
-    region: 'us-east-2',
+    credentials: {
+      accessKeyId: process.env.ACCESS_KEY_ID,
+      secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    },
+    region: 'us-east-2'
   },
   apiVersion: '2012-08-10'
 };
--- controllers/movies.js
+++ controllers/movies.js
@@ -1,62 +1,59 @@
-const AWS = require('aws-sdk');
+const { DynamoDBClient, ScanCommand, PutItemCommand } = require("@aws-sdk/client-dynamodb");
 const config = require('./../config.js');
-// const uuidv1 = require('uuid/v1');
 
 const { v1: uuidv1 } = require('uuid');
 
-const getMovies = function (req, res) {
-    AWS.config.update(config.aws_remote_config);
-
-    // Create the DynamoDB service object
-    const ddb = new AWS.DynamoDB({ apiVersion: config.apiVersion });
+const getMovies = async (req, res) => {
+    try {
+        const client = new DynamoDBClient(config.aws_remote_config);
     
-    var params = {
-        TableName: config.aws_table_name,
-    };
+        const input = {
+            TableName: config.aws_table_name,
+        };
 
-    ddb.scan(params, function (err, data) {
-        if (err) {
-             res.send({
-                success: false,
-                message: err
-             });
-            return;
-        }
+        const command = new ScanCommand(input);
+        const response = await client.send(command);
+
+        const { Items: movies } = response;
 
         res.send({
             success: true,
-            message: 'movies',
-            movies: data
+            message: 'Movies',
+            movies
         });
-    });
+
+    } catch (err) {
+        res.send({
+            success: false,
+            message: err
+        });
+    }
 }
 
-const addMovie = function (req, res) {
-    AWS.config.update(config.aws_remote_config);
-    const docClient = new AWS.DynamoDB.DocumentClient();
-    const Item = { ...req.body };
-    Item.id = uuidv1();
-    
-    var params = {
-        TableName: config.aws_table_name,
-        Item: Item
-    };
+const addMovie = async (req, res) => {
+    try {
+        const client = new DynamoDBClient(config.aws_remote_config);
 
-    // Call DynamoDB to add the item to the table
-    docClient.put(params, function (err, data) {
-        if (err) {
-            res.send({
-                success: false,
-                message: err
-            });
-        } else {
-            res.send({
-                success: true,
-                message: 'Added movie',
-                movie: data
-            });
-        }
-    });
+        const Item = { ...req.body, id: { S: uuidv1()} };
+
+        const input = {
+            TableName: config.aws_table_name,
+            Item: Item
+        };
+    
+        const command = new PutItemCommand(input);
+        await client.send(command);
+
+        res.send({
+            success: true,
+            message: 'Added movie',
+        });
+    } catch (err) {
+          res.send({
+            success: false,
+            message: err
+        });
+    }
 }
 
 module.exports = {
